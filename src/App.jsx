26d7fe2f5e1657f177c17083ea8ef439d62f3895
REBECCA FRENCH <rlfweb8@gmail.com>
2022-11-22 14:51:28 +0000

    Fetch data from api and store in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 import logo from './logo.svg'
 
 const App = () => {
+  const [books, setBooks] = useState([])
+
+  console.log('this message is going to load every time the component renders')
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch('https://book-club-json.herokuapp.com/books')
+        .then((response) => {
+          console.log(`here's what our fetch request returns`, response)
+          return response.json()
+        })
+        .then((books) => {
+          console.log(`our json-ified response: `, books)
+          return setBooks(books)
+        })
+    }
+    fetchData()
+  }, [])
+
+  console.log(`the books array in our state`, books)
+
   return <div>Hello Rebecca</div>
 }
 
