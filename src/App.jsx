21c0ef1daaaafa21c20da51db071ea06c0fbd52e
REBECCA FRENCH <rlfweb8@gmail.com>
2022-12-15 20:51:35 +0000

    Removal of console logs

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,15 @@ import BooksContainer from './components/BooksContainer'
 const App = () => {
   const [books, setBooks] = useState([])
 
-  console.log('this message is going to load every time the component renders')
-
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch('https://book-club-json.herokuapp.com/books')
-        console.log(`here's what our fetch request returns`, response)
-
-        const books = await response.json()
-        console.log(`our json-ified response: `, books)
-        setBooks(books)
-      } catch (errors) {
-        console.log(errors)
-      }
+      const response = await fetch('https://book-club-json.herokuapp.com/books')
+      const books = await response.json()
+      setBooks(books)
     }
 
     fetchData()
   }, [])
-
-  console.log(`the books array in our state`, books)
 
   return (
     <>
